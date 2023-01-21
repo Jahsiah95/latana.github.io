5349d653178555252a4328c5b2280125b3b85db4
Jahsiah95 <104687455+Jahsiah95@users.noreply.github.com>
2023-01-21 17:21:40 +0000

    Delete Site2 directory

--- Site2/Events/events.js
+++
@@ -1,8 +0,0 @@
-const tickets = Array.from(document.querySelectorAll('.ticketButton'));
-tickets.forEach((ticketButton)=>{
-    ticketButton.addEventListener('click',ticketLink)
-})
-
-function ticketLink(){
-    window.open('https://buytickets.designmynight.com?item-ids=6325d37bd46a3b4d6a5bed8b&checkoutMode=blank');
-}
