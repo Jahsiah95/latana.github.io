a77ec3e1dcd577c1a452b4ea81470b15dd0ea465
Jahsiah <jahsiah.c@hotmail.co.uk>
2023-02-10 00:10:06 +0000

    final tweaks

--- Gallery/gallery.js
+++ Gallery/gallery.js
@@ -17,3 +17,30 @@ window.onclick = function(event){
         }
     }
 }
+
+
+//Slideshow
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n){
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n){
+    showSlides(slideIndex = n)
+}
+
+function showSlides(n){
+    let slides = document.querySelectorAll(".slide");
+    if(n > slides.length){
+        slideIndex = 1;
+    }
+    if(n < 1){
+        slideIndex = slides.length;
+    }
+    for(let i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "flex";
+}
